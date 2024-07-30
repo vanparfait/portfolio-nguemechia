f0a8bae1d8e605db421d345fb9dad9beee42c343
vanparfait <nguemechiaparfait@gmail.com>
2024-07-30 09:15:39 +0100

    initialisation de mon projet portfolio,installation des differntes bibliotheques et supression des fichiers indesirables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,36 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      },
+      maxWidth: {
+        container: "1440px",
+        contentContainer: "1140px",
+        containerSmall: "1024px",
+        containerxs: "768px",
+      },
+      fontFamily: {
+        bodyFont: ["Montserrat", "sans-serif"],
+        titleFont: ["Inter", "sans-serif"],
+      },
+      boxShadow: {
+        navbarShadow: "0 10px 30px -10px rgba(2,12,27.0.7)",
+      },
+      colors: {
+        bodycolor: "#0A192F",
+        textGreen: "#64FFDA",
+        textLight: "#CCD6F6",
+        textDark: "#8892B0",
+        hoverColor: "rgba(100,255,218,0.1)",
+      },
     },
   },
   plugins: [],
